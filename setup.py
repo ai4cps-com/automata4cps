0e384ea469d9f9e8d5a7c062af33b2d1a6a4891a
Nemanja <nemanjahrane@gmail.com>
2024-08-07 17:20:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="automata4cps",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         # List your project's dependencies here, for example:
