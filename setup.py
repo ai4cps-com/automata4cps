0d2d12f840de5438d128d9face1729bfebf1d68e
Nemanja <nemanjahrane@gmail.com>
2024-08-22 16:03:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setup(
     packages=find_packages(),
     install_requires=[
         # List your project's dependencies here, for example:
-        'dash','pandas', 'networkx', 'plotly', 'numpy', 'dash_daq', 'dash-bootstrap-components', 'pydotplus',
-        'dash-cytoscape', 'simpy', 'mlflow', 'torch'
+        'dash', 'pandas', 'networkx', 'plotly', 'numpy', 'dash_daq', 'dash-bootstrap-components', 'pydotplus',
+        'dash-cytoscape', 'simpy', 'mlflow', 'torch', 'z3', 'scipy', 'sphinx'
     ],
     author="Nemanja Hranisavljevic & Tom Westermann",
     author_email="nemanja@ai4cps.com",
-    description="Tools for learning, plotting, analyzing etc of discrete, timed and hybrid automata.",
+    description="Tools for learning, plotting, analyzing etc. of discrete, timed and hybrid automata.",
     url="https://github.com/ai4cps-com/automata4cps",
     classifiers=[
         "Programming Language :: Python :: 3",
